2985f1144d28ddfbc48b0e5f5b6509378cc0d4ec
Max Garro Mora <maxgarro22@gmail.com>
2025-06-30 17:01:05 -0600

    [Update] Cambios logger

--- API/utils/database/Artist/rate_artist_db.py
+++ API/utils/database/Artist/rate_artist_db.py
@@ -4,26 +4,6 @@ from utils.database.database import db
 from utils.logging.logger import configurar_logger
 import traceback
 
-<<<<<<< HEAD
-def ejecutar_rate_artist_DB(fan_id, artist_id, rating):
-    try:
-        sql = text("SELECT * FROM rate_artist(:fan_id, :artist_id, :rating)")
-        result = db.session.execute(sql, {
-            "fan_id": fan_id,
-            "artist_id": artist_id,
-            "rating": rating
-        })
-        
-        output = result.scalar()
-        
-        db.session.commit()
-        
-        return output
-    except Exception as e:
-        db.session.rollback()
-        print(f"Error al calificar artista: {e}")
-        return None
-=======
 logger = configurar_logger()
 
 def ejecutar_rate_artist_DB(valid_data):
@@ -54,4 +34,3 @@ def ejecutar_rate_artist_DB(valid_data):
             "detalle": str(e),
             "trace": traceback.format_exc()
         }), 500
->>>>>>> API_CalificarArtista
